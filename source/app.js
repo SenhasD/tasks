57b1fb132960af9a2764007d69c466224dc7a86b
SenhasD <senhasdiwan@gmail.com>
2024-07-21 17:06:57 +0530

    New features and some bug fixes

--- source/app.js
+++ source/app.js
@@ -1,13 +1,21 @@
 let noteRef = 1;
+if (localStorage.getItem("Tasks") !== null) {
+    console.log("Recently used tasks:");
+    console.log(localStorage.getItem("Tasks"));
+    console.log(localStorage.getItem("ID"));
+} else {
+    console.log("No recent data found");
+};
 
 function fetch() {
     let newTask = document.getElementById("data").value;
-    localStorage.setItem("Tasks", newTask);
     let data = localStorage.getItem("Tasks");
     document.getElementById('default').textContent = "";
     let container = document.querySelector(".update");
     let AppendNote = document.createElement('p');
     let noteID = noteRef++;
+    localStorage.setItem("Tasks", newTask);
+    localStorage.setItem("ID", noteID);
     AppendNote.id = noteID;
     AppendNote.textContent = data + " | Note ID: " + noteID;
     container.appendChild(AppendNote);
@@ -20,5 +28,23 @@ function delNote() {
         removeNote.remove();
     } else {
         alert("Note does not exist")
+    };
+};
+
+function deleteRecent() {
+    localStorage.clear();
+    console.log("Local browser storage cleared");
+};
+
+function restoreTask() {
+    let container = document.querySelector(".update");
+    let AppendNote = document.createElement('p');
+    let data = localStorage.getItem("Tasks");
+    let noteID = localStorage.getItem("ID");
+    if (data !== null) {
+        AppendNote.textContent = data + " | Note ID: " + noteID;
+        container.appendChild(AppendNote);
+    } else {
+        alert("No recent note found");
     };
 };
